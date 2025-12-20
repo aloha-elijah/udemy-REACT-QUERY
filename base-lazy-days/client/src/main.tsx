16d721c95c9f48c450cb9cd13cef98657e0c7d1d
Bonnie Schulkin <biz@bonnieschulkin.com>
2025-12-20 10:37:13 -0800

    Remove unnecessary duplicate theme provider

--- base-lazy-days/client/src/main.tsx
+++ base-lazy-days/client/src/main.tsx
@@ -1,8 +1,5 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { theme } from "./theme";
 
 import { App } from "@/components/app/App";
 
@@ -10,9 +7,7 @@ const root = document.getElementById("root");
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
+      <App />
     </React.StrictMode>
   );
 }
