95371523d0da414aae37ed9df6d1056f0f15c81f
elijah lee <84287663+aloha-elijah@users.noreply.github.com>
2026-01-10 17:21:26 -0800

    Integrate React Query for infinite people loading
    Wrapped the app in QueryClientProvider and refactored InfinitePeople to use useInfiniteQuery from React Query for fetching paginated people data from SWAPI. This enables efficient infinite scrolling and better data management.

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,23 @@
 import "./App.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,44 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = "https://swapi.py4e.com/api/people/";
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
+  const { isError, isLoading, isFetching, data, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: ["sw-peope"],
+      queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    });
+
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return (
+    <InfiniteScroll
+      loadMore={() => {
+        if (!isFetching) fetchNextPage();
+      }}
+      hasMore={hasNextPage || false}
+    >
+      {data?.pages.map((page, pageIndex) => (
+        <div key={pageIndex}>
+          {page.results.map((person) => (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            />
+          ))}
+        </div>
+      ))}
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error fetching data.</div>}
+    </InfiniteScroll>
+  );
 }
